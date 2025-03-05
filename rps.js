db07fc851475ce6ceac2e040f1a6b97dd688d6d6
rel <158558115+jysgu@users.noreply.github.com>
2025-03-05 17:01:46 +0800

    add playRound function and semicolon to return choice
    added playRound function for the logic of how the round will go between the player and the computer to determine who will win.

--- rps.js
+++ rps.js
@@ -16,9 +16,35 @@ function getComputerChoice() {
 	console.log("");
 }
 
-function getHumanChoice(){
-	let choice = prompt("Choose between Rock, Paper or Scissors!").toLowerCase();
-	return choice
+function getHumanChoice() {
+	let choice = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
+	return choice;
 }
 
+function playRound() {
+	let humanChoice = getHumanChoice();
+	let computerChoice = getComputerChoice();
 
+	console.log(`Player choice: ${humanChoice} \nComputer choice: ${computerChoice}`);
+
+	if (humanChoice === null || humanChoice === "" || !/^(rock|paper|scissors)$/.test(humanChoice)){
+		alert("Only type Rock, Paper or Scissors!");
+		return;
+	}
+
+	if (humanChoice === computerChoice) {
+		console.log("It's a draw!");
+	} else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+	(humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors"
+	&& computerChoice === "paper")) {
+		console.log(`${humanChoice} beats ${computerChoice}! You win!`);
+		humanScore++;
+	} else {
+		console.log(`${computerChoice} beats ${humanChoice}! You lose!`);
+		computerScore++;
+	}
+
+	console.log(`Player score: ${humanScore} Computer Score: ${computerScore}`);
+}
+
+playRound();
